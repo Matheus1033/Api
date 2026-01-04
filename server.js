adc23cbfb45d2a08353b75c9ecbc4e014ab96f40
Aquino0 <cristopheraquino20@gmail.com>
2026-01-04 11:11:08 -0300

    Baixando o prisma e preparando pra adicionar um banco de dados ao projeto

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const users = [];
 app.post("/users", (req, res) => {
   users.push(req.body);
 
-  res.send("Ok, Posted");
+  res.status(201).json(req.body);
 });
 
 app.get("/users", (req, res) => {
-  res.json(users);
+  res.status(200).json(users);
 });
 
 app.listen(3000);
