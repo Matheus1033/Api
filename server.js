7a32bf914872c25aaae025ec406a3e37fea92bc3
Aquino0 <cristopheraquino20@gmail.com>
2026-01-04 10:42:34 -0300

    Testando o metodo post da api

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+const users = [];
+
+app.post("/users", (req, res) => {
+  console.log(req);
+
+  res.send("Ok, Posted");
+});
+
 app.get("/users", (req, res) => {
   res.send("Olá, Mundo!");
 });
